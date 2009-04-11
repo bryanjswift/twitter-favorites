3b00d8159ab9d23ad91bcfdb9756ba5964d8813d
Bryan J Swift <bryan@bryanjswift.com>
2009-04-10 23:50:10 -0400

    basic source files taken from http://mawson.wordpress.com/2009/04/10/first-steps-with-scala-on-google-app-engine/

---
+++ src/main/scala/com/bryanjswift/twitterFavorites/LoginSnippet.scala
@@ -0,0 +1,19 @@
+package com.bryanjswift.twitterFavorites
+
+object LoginSnippet {
+    def render(request: _root_.javax.servlet.http.HttpServletRequest) = {
+        import _root_.com.google.appengine.api.users._
+        val userService = UserServiceFactory.getUserService
+        val user = userService.getCurrentUser
+
+        if (user == null) {
+            <p>Hello! <a href={userService.createLoginURL
+                (request.getRequestURI)}>Sign in</a>
+                just for kicks.</p>
+        } else {
+            <p>Hello, {user.getNickname}! Now you can
+                <a href={userService.createLogoutURL(
+                request.getRequestURI)}>sign out</a> again.</p>
+        }
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.bryanjswift.twitterFavorites.LoginSnippet" %>
+<html>
+  <body>
+    <h1>Just a test</h1>
+    <%= LoginSnippet.render(request) %>
+  </body>
+</html>
+
