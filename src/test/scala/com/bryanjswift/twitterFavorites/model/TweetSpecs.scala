75c0e883dd16d725c57b3ddf622ce46972d510d4
Bryan J Swift <bryan@bryanjswift.com>
2009-04-12 15:58:07 -0400

    added a number of specs for the Tweet class

--- src/test/scala/com/bryanjswift/twitterFavorites/model/TweetSpecs.scala
+++ src/test/scala/com/bryanjswift/twitterFavorites/model/TweetSpecs.scala
@@ -7,15 +7,46 @@ import scala.xml.XML
 class tweetTest extends JUnit4(tweetTests)
 
 object tweetTests extends Specification {
-	val favorites = XML.load(getClass.getResourceAsStream("/favorites.xml"))
-	val tweet = new Tweet((favorites \\ "status").apply(1))
 	def have = addToSusVerb("have")
 	"tweet" should have {
+		val favorites = XML.load(getClass.getResourceAsStream("/favorites.xml"))
+		val tweet = new Tweet((favorites \\ "status")(1))
 		"an id with all digits" in {
 			tweet.id matches """\d+"""
 		}
 		"a message less than or equal to 140 characters" in {
 			tweet.message.length <= 140
+		}
+	}
+	"status with text consisting of only 1 url" should have {
+		val status = <status><id>7865234</id><text>http://lift-example.appspot.com/index</text></status>
+		val tweet = new Tweet(status)
+		val urls = tweet.getUrls
+		"getUrls length of 1" in {
+			urls.length == 1
+		}
+		"first element of getUrls equal to status.text" in {
+			urls(0) == (status \ "text").text
+		}
+	}
+	"status with text consisting of only 2 urls" should have {
+		val status = <status><id>7865234</id><text>http://bryanjswift.com http://lift-example.appspot.com/index</text></status>
+		val tweet = new Tweet(status)
+		val urls = tweet.getUrls
+		"getUrls length of 2" in {
+			urls.length == 2
+		}
+		"getUrls with the same values as status.text.split" in {
+			val texts = (status \ "text").text.split(" ")
+			texts(0) == urls(0) && texts(1) == urls(1)
+		}
+	}
+	"status with text consisting of words and 2 urls" should have {
+		val status = <status><id>7865234</id><text>these are http://bryanjswift.com some stupid http://lift-example.appspot.com/index links</text></status>
+		val tweet = new Tweet(status)
+		val urls = tweet.getUrls
+		"getUrls length of 2" in {
+			urls.length == 2
 		}
 	}
 }
