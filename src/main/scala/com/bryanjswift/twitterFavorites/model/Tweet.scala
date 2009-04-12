490b4b11e301f5b5705a508b5fffeff8d1949425
Bryan J Swift <bryan@bryanjswift.com>
2009-04-12 16:05:38 -0400

    added a toString method to the Tweet class

--- src/main/scala/com/bryanjswift/twitterFavorites/model/Tweet.scala
+++ src/main/scala/com/bryanjswift/twitterFavorites/model/Tweet.scala
@@ -10,9 +10,12 @@ class Tweet(val status:Node) {
 	val id = (status \ "id").text
 	val message = (status \ "text").text
 	val createdOn = new Date()
-	def getUrls(): List[String] = {
+	def getUrls:List[String] = {
 		// regex taken from http://snipplr.com/view/2371/regex-regular-expression-to-match-a-url/
 		val urlPattern = new Regex("""https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?""")
 		urlPattern findAllIn message toList
+	}
+	override def toString:String = {
+		"[ id: " + id + ", message: " + message + " ]"
 	}
 }
