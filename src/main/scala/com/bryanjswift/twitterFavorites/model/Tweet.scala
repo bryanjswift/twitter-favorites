255d59b82a199710fde8d7b70d2590cdf75cb8da
Bryan J Swift <bryan@bryanjswift.com>
2009-04-12 15:57:50 -0400

    changed method signature of Tweet.getUrls to return a List of Strings rather than an Iterator of Strings - added a createdOn field which is set to the current date/time when initialized

--- src/main/scala/com/bryanjswift/twitterFavorites/model/Tweet.scala
+++ src/main/scala/com/bryanjswift/twitterFavorites/model/Tweet.scala
@@ -1,5 +1,7 @@
 package com.bryanjswift.twitterFavorites.model
 
+import java.util.Date
+
 import scala.util.matching.Regex
 import scala.xml.Node
 import scala.xml.NodeSeq
@@ -7,9 +9,10 @@ import scala.xml.NodeSeq
 class Tweet(val status:Node) {
 	val id = (status \ "id").text
 	val message = (status \ "text").text
-	def getUrls() : Iterator[String] = {
+	val createdOn = new Date()
+	def getUrls(): List[String] = {
 		// regex taken from http://snipplr.com/view/2371/regex-regular-expression-to-match-a-url/
 		val urlPattern = new Regex("""https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?""")
-		urlPattern findAllIn message
+		urlPattern findAllIn message toList
 	}
 }
