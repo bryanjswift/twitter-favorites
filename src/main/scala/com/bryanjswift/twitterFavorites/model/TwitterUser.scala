1241e44f47c4a019e0ea363c1b40a7d34f9d6428
Bryan J Swift <bryan@bryanjswift.com>
2009-04-12 23:47:05 -0400

    updated the TwitterUser class to properly fetch tweets from Twitter - added specs to check the behavior of TwitterUser.fetchTweets - added dispatch and its dependencies back to pom.xml - added information about building dispatch to README - added information about installing dispatch and it's dependencies to the local Maven repository

--- src/main/scala/com/bryanjswift/twitterFavorites/model/TwitterUser.scala
+++ src/main/scala/com/bryanjswift/twitterFavorites/model/TwitterUser.scala
@@ -1,9 +1,21 @@
 package com.bryanjswift.twitterFavorites.model
 
+import dispatch.Http
 import scala.collection.mutable.ListBuffer
+import scala.xml.NodeSeq
 
 class TwitterUser(val username:String) {
 	require(username != null && username.length > 0)
 	val tweets = new ListBuffer[Tweet]()
-	def fetchTweets:Unit = { }
+	val request = (new Http("twitter.com"))("/favorites/" + username + ".xml")
+	def fetchTweets:Seq[Tweet] = {
+		def processTweets(nodes:NodeSeq):Unit = {
+			(nodes \\ "status").elements.foreach((status) => {
+				tweets += new Tweet(status)
+			})
+		}
+		val lastId = if (!tweets.isEmpty) tweets.last.id else null
+		if (lastId == null) request <> processTweets else request ?< Map("since_id" -> lastId) <> processTweets
+		tweets.filter((tweet) => lastId == null || tweet.id > lastId)
+	}
 }
--- src/test/scala/com/bryanjswift/twitterFavorites/model/TwitterUserSpecs.scala
+++ src/test/scala/com/bryanjswift/twitterFavorites/model/TwitterUserSpecs.scala
@@ -25,7 +25,24 @@ object TwitterUserSpecs extends Specification {
 		}
 	}
 	"TwitterUser has an empty list of tweets when initialized" in {
-		val user = new TwitterUser("dpp")
+		val user = new TwitterUser("bryanjswift")
 		user.tweets != null && user.tweets.length == 0
+	}
+	"TwitterUser.fetchTweets" should {
+		val user = new TwitterUser("bryanjswift")
+		val tweets = user.fetchTweets
+		"not be empty" in {
+			!tweets.isEmpty
+		}
+		"have a size less than or equal to 20" in {
+			tweets.length <= 20 && tweets.length >= 1
+		}
+		"have the same number of tweets in it as in the TwitterUser.tweets property" in {
+			user.tweets.length == tweets.length
+		}
+		"produce an empty result if called again immediately" in {
+			val moreTweets = user.fetchTweets
+			moreTweets.length == 0
+		}
 	}
 }
