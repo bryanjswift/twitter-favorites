5d803bbe29759db5be76d432bfdda42b44d7da95
Bryan J Swift <bryan@bryanjswift.com>
2009-04-17 01:19:37 -0400

    some revisions to the Http class

--- src/main/scala/com/bryanjswift/http/Http.scala
+++ src/main/scala/com/bryanjswift/http/Http.scala
@@ -1,13 +1,21 @@
 package com.bryanjswift.http
 
-import com.google.appengine.api.urlfetch.{HTTPRequest,URLFetchService,URLFetchServiceFactory}
+import com.google.appengine.api.urlfetch.{FetchOptions,HTTPMethod,HTTPRequest,URLFetchService,URLFetchServiceFactory}
+import java.net.URL
 
-class Http(val host:String) {
+class Http(scheme:String, host:String, port:Int) {
+	def this(scheme:String, host:String) = this(scheme, host, 80)
+	def this(host:String, port:Int) = this("http", host, port)
+	def this(host:String) = this("http", host, 80)
+
 	def apply(uri:String) = new Request(uri)
 
-	class Request(req:HTTPRequest)
+	class Request(req:HTTPRequest) {
+		def this(uri:String) = this(new HTTPRequest(
+			new URL(scheme, host, port, uri), HTTPMethod.GET, FetchOptions.Builder.disallowTruncate))
+	}
 }
 
 object Http {
-	def apply(host:String) = new Http(domain)
+	def apply(host:String) = new Http(host)
 }
